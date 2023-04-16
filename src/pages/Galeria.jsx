2954e692b975fff40e552db2086189caf219c355
Gonzalo Mos <gon.mb3@gmail.com>
2023-04-15 17:37:51 -1100

    gallery

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -22,10 +22,8 @@ const Galeria = () => {
         preload: [0,1] // Will preload 0 - before current, and 1 after the current image
       },
       image: {
-        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-        titleSrc: function(item) {
-          return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-        }
+        tError: '',
+        
       }
     });
   });
